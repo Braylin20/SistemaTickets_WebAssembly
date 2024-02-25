b885870d47e90fdd28c5318a10cb555019ff6b1d
Braylin <braylinvasquez20@gmail.com>
2024-02-25 13:48:23 -0400

    Creando el contexto

---
+++ SistemaTicketsAssembly.Api/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaTicketsAssembly.Models;
+
+namespace SistemaTicketsAssembly.DAL
+{
+    public class Contexto :DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) :base(options) { }
+        public DbSet<Tickets> Tickets { get; set; }
+    }
+}
