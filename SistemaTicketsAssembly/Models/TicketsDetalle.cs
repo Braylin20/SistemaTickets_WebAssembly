c9537e0cdd77da872814673d5cf678df06c65a93
Braylin <braylinvasquez20@gmail.com>
2024-02-25 13:19:58 -0400

    Agreando el modelo Tickets y TicketsDetalle

---
+++ SistemaTicketsAssembly/Models/TicketsDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaTicketsAssembly.Models
+{
+    public class TicketsDetalle
+    {
+        [Key]
+        public int DetalleId {  get; set; }
+        [Required(ErrorMessage ="Este campo es requerido")]
+        [ForeignKey("TicketId")]
+        public int TicketId { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Emisor { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Mensaje { get; set; }
+
+
+    }
+}
