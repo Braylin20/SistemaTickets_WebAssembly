d1f34b9aea4202a1b04428fc24cb91cd2d6737bf
Braylin <braylinvasquez20@gmail.com>
2024-02-25 13:50:12 -0400

    Creando el conecction string e injectando el contexto al program

--- SistemaTicketsAssembly.Api/Program.cs
+++ SistemaTicketsAssembly.Api/Program.cs
@@ -1,3 +1,8 @@
+
+
+using Microsoft.EntityFrameworkCore;
+using SistemaTicketsAssembly.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var Constr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Contexto>(option=>option.UseSqlite(Constr));
 
 var app = builder.Build();
 
