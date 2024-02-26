167d0bbafae9ea8868e90f82cc0e9a34d561f631
Braylin <braylinvasquez20@gmail.com>
2024-02-26 16:03:12 -0400

    Agregando el DbSet de TicketDetalle

--- SistemaTicketsAssembly.Api/DAL/Contexto.cs
+++ SistemaTicketsAssembly.Api/DAL/Contexto.cs
@@ -7,5 +7,6 @@ namespace SistemaTicketsAssembly.DAL
     {
         public Contexto(DbContextOptions<Contexto> options) :base(options) { }
         public DbSet<Tickets> Tickets { get; set; }
+        public DbSet<TicketsDetalle> TicketsDetalle { get; set; }
     }
 }
