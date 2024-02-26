09417096ff249769a22c298e5f25783d7f8f88b3
Braylin <braylinvasquez20@gmail.com>
2024-02-26 16:06:46 -0400

    Haciendo la migracion de TicketDetalle

--- SistemaTicketsAssembly.Api/Migrations/20240226192044_TicketDetalles.Designer.cs
+++ SistemaTicketsAssembly.Api/Migrations/20240226192044_TicketDetalles.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaTicketsAssembly.DAL;
 
@@ -10,9 +11,11 @@ using SistemaTicketsAssembly.DAL;
 namespace SistemaTicketsAssembly.Api.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20240226192044_TicketDetalles")]
+    partial class TicketDetalles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.2");
---
+++ SistemaTicketsAssembly.Api/Migrations/20240226192044_TicketDetalles.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaTicketsAssembly.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class TicketDetalles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TicketsDetalle",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TicketId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Emisor = table.Column<string>(type: "TEXT", nullable: false),
+                    Mensaje = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketsDetalle", x => x.DetalleId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TicketsDetalle");
+        }
+    }
+}
